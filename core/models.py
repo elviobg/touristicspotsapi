5a9f2aa55a829cfb0c303409f01d694452ae023d
Elvio <elviobg@gmail.com>
2018-07-30 15:42:04 -0300

    Atualiza o modelo do ponto turistico

--- core/models.py
+++ core/models.py
@@ -8,10 +8,10 @@ class Spot(models.Model):
   name = models.CharField(max_length=256)
   description = models.TextField()
   approved = models.BooleanField(default=False)
-  attractions = models.ManyToManyField(Attraction)
-  comments = models.ManyToManyField(Comment)
-  review = models.ManyToManyField(Review)
-  localization = models.ForeignKey(Localization, on_delete=models.CASCADE)
+  attractions = models.ManyToManyField(Attraction, null=True, blank=True)
+  comments = models.ManyToManyField(Comment, null=True, blank=True)
+  review = models.ManyToManyField(Review, null=True, blank=True)
+  localization = models.ForeignKey(Localization, on_delete=models.CASCADE, null=True, blank=True)
 
   def __str__(self):
     return self.name

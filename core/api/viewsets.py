c880469c4cc14a9b2450363144671df0394dce85
Elvio <elviobg@gmail.com>
2018-07-31 17:19:23 -0300

    Adiciona autenticação e permissão ao endpoint de  ponto turistico

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets
-from core.models import Spot
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
+from core.models import Spot
 from .serializers import SpotSerializer
 
 
 class SpotViewSet(viewsets.ModelViewSet):
     serializer_class = SpotSerializer
     filter_backends = (DjangoFilterBackend,)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, )
     filter_fields = ('name', 'description', )
-
+    
     def get_queryset(self):
         return Spot.objects.all().filter(approved=True)
 
@@ -16,3 +20,6 @@ class SpotViewSet(viewsets.ModelViewSet):
     # list, create, destroy, retrieve, update, partial_update
     def list(self, request, *args, **kwargs):
         return super(SpotViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super(SpotViewSet, self).retrieve(request, *args, **kwargs)

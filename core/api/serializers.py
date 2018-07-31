c36db4f7b2db8c4b827596127f41fd989dff193c
Elvio <elviobg@gmail.com>
2018-07-31 17:58:39 -0300

    Serializa os objetos utilizados em pontos turisticos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import Spot
+from attraction.api.serializers import AttractionSerializer
+from comments.api.serializers import CommentSerializer
+from reviews.api.serializers import ReviewSerializer
+from localization.api.serializers import LocalizationSerializer
 
 
 class SpotSerializer(ModelSerializer):
+    attractions = AttractionSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    review = ReviewSerializer(many=True)
+    localization = LocalizationSerializer()
+    
     class Meta:
         model = Spot
         fields = '__all__'

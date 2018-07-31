2c48fd43812abfa401d583e36e7ec9a3769c70d9
Elvio <elviobg@gmail.com>
2018-07-30 22:56:38 -0300

    Adiciona filtros para os parametros da url

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -7,7 +7,20 @@ class SpotViewSet(viewsets.ModelViewSet):
     serializer_class = SpotSerializer
 
     def get_queryset(self):
-      return Spot.objects.all().filter(approved=True)
+      params = self.request.query_params
+      pk = params.get('id', None)
+      name = params.get('name', None)
+      description = params.get('description', None)
+      querySet = Spot.objects.all().filter(approved=True)
+
+      if pk:
+        querySet = querySet.filter(pk=pk)
+      if name:
+        querySet = querySet.filter(name__icontains=name)
+      if description:
+        querySet = querySet.filter(description__icontains=description)
+
+      return querySet
 
     #método de sobrescrita das chamadas padrão
     #list, create, destroy, retrieve, update, partial_update
